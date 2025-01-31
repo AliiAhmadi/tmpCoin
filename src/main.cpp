a6f3674c9e0e382d3fe6c27dd3dd1c5dd44181b7
lexur <aliahmadi.github@gmail.com>
2025-01-31 22:27:42 +0330

    delete crow and use httplib for simplicity

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,32 @@
 /** */
-#include "crow.h"
+#include "httplib.h"
 #include "tmpCoin.cpp"
+#include <uuid/uuid.h>
+
+std::string node_uuid() {
+  uuid_t uuid;
+  char uuidStr[37];
+
+  uuid_generate_random(uuid);
+  uuid_unparse(uuid, uuidStr);
+  return std::string(uuidStr);
+}
 
 int main(void) {
 
-  crow::SimpleApp app;
+  httplib::Server server;
+
+  server.Get("/", [](const httplib::Request &req, httplib::Response &res) {
+    res.set_content("Hello, World!", "text/plain");
+  });
+
+  std::string uuid = node_uuid();
 
   auto x = new tmpCoin{};
   auto trx = new Transaction("ali", "reza", 2.1);
   x->new_trx(trx);
 
-  // std::cout << x->new_block() << std::endl;
+  server.listen("0.0.0.0", 3000);
 
   return EXIT_SUCCESS;
 }
