ce34dce7d4402e40907d4434f903094d760b0623
lexur <aliahmadi.github@gmail.com>
2025-01-31 13:11:44 +0330

    make template

---
+++ scripts/format.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SRC_DIR="src"
+
+STYLE="file"
+
+find "$SRC_DIR" -type f \( -name "*.cpp" -o -name "*.h" \) -exec clang-format -i --style=$STYLE {} +
+
+echo "C++ files in '$SRC_DIR' formatted successfully."
